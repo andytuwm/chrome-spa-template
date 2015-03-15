0e97f47d9d64bd5c6c09f69f8e59f94beb1f08a4
Andy Tu <andytu@alumni.ubc.ca>
2015-03-14 21:10:32 -0700

    add comments

--- main.js
+++ main.js
@@ -19,55 +19,55 @@ template.menu = [
   {name: 'Menu 3', hash: 'menu3'}
 ];
 
+// Initializing function that runs once the template has been prepped for databinding
 template.addEventListener('template-bound', function(e) {
   this.route = this.route || DEFAULT_ROUTE; // Select initial route.
   currentMenuDisplayed = this.menu[0].hash;
   createMenuFrag(currentMenuDisplayed); // Initialize Main View
 });
 
+// Function that runs on menu item click in the scaffold drawer
 template.menuItemSelected = function(e) {
+  // Check what menu item was selected
   var menuNode = e.target.attributes.hash.nodeValue;
   console.log(menuNode);
 
+  // If selected item is the same one, no need to change the DOM
   if (menuNode != currentMenuDisplayed) {
 
+    // If selected menu item does not have a fragment, create and append.
+    // Customize each view accordingly
     if (menuNode == this.menu[0].hash && typeof fragmentcache_1 === 'undefined') {
-
       fragmentcache_1 = createMenuFrag(menuNode);
-
     } else if (menuNode == this.menu[1].hash && typeof fragmentcache_2 === 'undefined') {
-
       fragmentcache_2 = createMenuFrag(menuNode);
-
     } else if (menuNode == this.menu[2].hash && typeof fragmentcache_3 === 'undefined') {
-
       fragmentcache_3 = createMenuFrag(menuNode);
-
-    } else if (menuNode == this.menu[0].hash) {
-
+    }
+    // If the selected menu item already has a fragment, load the fragment instead.
+    // Once again, customize each view aaccordingly.
+    else if (menuNode == this.menu[0].hash) {
       loadFrag(fragmentcache_1.cloneNode(true));
-
     } else if (menuNode == this.menu[1].hash) {
-
       loadFrag(fragmentcache_2.cloneNode(true));
-
     } else if (menuNode == this.menu[2].hash) {
-
       loadFrag(fragmentcache_3.cloneNode(true));
-
     }
     currentMenuDisplayed = menuNode;
   }
 
-  template.route = DEFAULT_ROUTE;
+  template.route = DEFAULT_ROUTE; // route the user back to main view
   document.querySelector('#scaffold').closeDrawer();
 };
 
-// Functions
+/* Functions */
+
+// Duplicate this function for each menu UI
 function createMenuFrag(menuNode){
   var mainview = document.querySelector('#mainview');
   var mainviewChild = document.querySelector('#' + mvChildId);
 
+  // create DOM fragment to manipulate and then append
   var menufrag = document.createDocumentFragment();
   var menu = document.createElement('p');
   menu.id = mvChildId;
@@ -75,24 +75,27 @@ function createMenuFrag(menuNode){
   menufrag.appendChild(menu);
   fadeReplace(menufrag,mainviewChild,mainview);
 
-  return menufrag.cloneNode(true);
+  return menufrag.cloneNode(true); // clone the node for reuse
 }
 
+// Handles fade in/out of the mainview menu views
 function fadeReplace(frag, child, parent) {
   if(template.route == 'one') {
-    child.classList.add('fade-out');
+    child.classList.add('fade-out'); // fade out
     setTimeout( function() {
       parent.replaceChild(frag,child);
       child = document.querySelector('#' + mvChildId);
-      child.classList.add('fade-in');
-    },275);
-  } else {
+      child.classList.add('fade-in'); // fade in
+    },275); // wait 275 ms before replacing to allow fadeout animation (300ms) to run
+  } else { // if not on main view, no need for fade out animation
     parent.replaceChild(frag,child);
     child = document.querySelector('#' + mvChildId);
     child.classList.add('fade-in');
   }
 }
 
+// Duplicate this function for each menu UI as well if certain views require
+// populating data (eg. with xhr)
 function loadFrag(frag) {
   var mainview = document.querySelector('#mainview');
   var mainviewChild = document.querySelector('#' + mvChildId);
